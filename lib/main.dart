258abc8108886b630fe07a25d6c18e2d27b219b5
akash <akash8992@gmail.com>
2022-08-02 01:30:23 +0530

    dart learning

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  //print ("hello");
+
+  //built in data type
+  // int cart = 10;
+  // print(cart); 
+
+  // double price=10;
+  // print (price);
+
+// bool is_completed=true;
+// print(is_completed);
+
+// bool is_login=false;
+// print(is_login);
+
+// var quantity=100;
+// print (quantity);
+// print(quantity.runtimeType);
+
+
+//check data type 
+// operater
+// var a=10;
+// var b=20;
+// var c=30;
+// var d=40;
+// print(a+b);
+// print(b-a);
+// print(a*b);
+// print(a/b);
+// print(a~/b);
+// print(c%b);
+//prefix and postfix increment and decrement operater
+// var i=3;
+// print(i);
+// print(++i);
+// print(i++);
+// print(i--);
+// print(i++);
+// print(i);
+
+
+//equality and relational operator
+// 
+
+// Logical operater
+/* 
+true && true = true
+true && false = false
+false && true false
+false && false = false
+true || true = true
+true || false = true 
+false || true = true
+false|| false = false
+*/
+// var a=100;
+// var b=100;
+// var c=200;
+// var d=200;
+// print ((a==b)&& (b==a));
+// print ((a==b)&& (c==a));
+// print ((d==c)&& (b==d));
+
+//type test operators
+// var name="sonam";
+// var number=10;
+// print (name is String);
+// print (name is! String);
+// print (number is int);
+
+//Assignment operator
+// var n=10;
+// n+=5;
+// n-=5;
+// n*=5;
+// print(n);
+
+
+//#condational operator
+
+//var is_login=true;
+// var is_login=false;
+// //condation ? expression1 : expression2 if condation true it is written expression 1and if folse expression 2
+// var user = is_login? 'sonam' : 'guest';
+// print (user);
+
+
+
+}
+
